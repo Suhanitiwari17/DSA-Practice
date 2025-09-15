5bbf7abae465d65d6cc73da44c24ab8742caf76e
Suhanitiwari17 <tiwarisuhani939@gmail.com>
2025-09-16 01:30:29 +0530

    Create code.java

---
+++ Recursion/Print n to 1/code.java
@@ -0,0 +1,13 @@
+public class print1ton {
+    public static void main(String[] args) {
+        print(6);
+    }
+    static void print(int n){
+        if(n==0){
+            return;
+        }
+        //print(n-1);
+        System.out.println(n);
+        print(n-1);
+    }
+}
