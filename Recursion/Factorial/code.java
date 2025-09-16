5e711c5fdac94eae9da08a067cd1807f3bdde8b5
Suhanitiwari17 <tiwarisuhani939@gmail.com>
2025-09-16 08:42:32 +0530

    Create code.java

---
+++ Recursion/Factorial/code.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number:");
+        int n=sc.nextInt();
+        int fact=1;
+        if(n==0||n==1){
+            System.out.println("1");
+        }
+        else{
+          for(int i=2;i<=n;i++){
+              fact=fact*i;
+           }
+           System.out.println(fact);
+        }
+}
+}
